61027070a3275fe029ca213211b242b5f23b497d
Vasyl Savchuk <safchug@gmail.com>
2020-11-24 10:57:04 +0200

    changed form, added validation

--- src/components/UpdateCityForm.vue
+++ src/components/UpdateCityForm.vue
@@ -1,5 +1,6 @@
 <template>
     <v-container >
+      <v-form v-model="valid" @submit.prevent="submit()" ref="form">
       <v-card
           class="mx-auto my-12"
           max-width="374"
@@ -8,18 +9,33 @@
         <template v-if="!err">
         <v-card-title >{{city.name}}</v-card-title>
         <v-card-text>
-          <v-row>
-            <strong>Location: </strong><v-text-field v-model="location"></v-text-field>
-          </v-row>
-          <v-row>
-            <strong>Population: </strong><v-text-field v-model="population"></v-text-field>
-          </v-row>
-          <v-row>
-            <strong>Area: </strong><v-text-field v-model="area"></v-text-field>
-          </v-row>
-          <v-row>
-            <strong>Found: </strong><v-text-field v-model="found"></v-text-field>
-          </v-row>
+            <v-row>
+              <v-text-field v-model="name"
+                            :rules="nameRules"
+                            label="Name">
+              </v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field v-model="location"
+                            :rules="locationRules" label="Location"
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field v-model="population"
+                            :rules="populationRules" label="Population"
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field v-model="area"
+                            :rules="areaRules"
+                            label="Area"
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field v-model="found"
+                            :rules="foundRules"
+                            label="Found"></v-text-field>
+            </v-row>
           <template v-if="updateStus">
             <v-row>
               <v-alert
@@ -29,9 +45,9 @@
         </v-card-text>
         <v-card-actions>
           <v-btn
+              type="submit"
               color="orange"
               text
-              @click="updateCity(city.id)"
           >
             Update
           </v-btn>
@@ -54,6 +70,7 @@
             </v-row>
           </template>
       </v-card>
+      </v-form>
     </v-container>
 </template>
 
@@ -68,7 +85,23 @@ export default {
     area: '',
     found: '',
     err: '',
-    updateStus: ''
+    updateStus: '',
+    valid: false,
+    nameRules: [
+      v => v.trim().length > 0 || 'Name is required'
+    ],
+    locationRules: [
+      v => v.trim().length > 0 || 'Location is required'
+    ],
+    populationRules: [
+      v => v.trim().length > 0 || 'Population is required'
+    ],
+    areaRules: [
+      v => v.trim().length > 0 || 'Area is required'
+    ],
+    foundRules: [
+      v => v.trim().length > 0 || 'Found is required'
+    ]
   }),
   computed: {
     ...mapState({
@@ -77,6 +110,12 @@ export default {
   },
   methods: {
     ...mapActions(['fetchCityWithId', 'updateCityWithId']),
+    submit(){
+      this.$refs.form.validate();
+      if(this.valid) {
+        this.updateCity(this.city.id);
+      }
+    },
     async updateCity(id) {
       let data = {
         location: this.location,
