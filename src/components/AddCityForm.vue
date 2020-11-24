c2c5740a3bf9baa8784396693677deee0cfeddb2
Vasyl Savchuk <safchug@gmail.com>
2020-11-24 10:45:26 +0200

    added validation

--- src/components/AddCityForm.vue
+++ src/components/AddCityForm.vue
@@ -1,28 +1,42 @@
 <template>
   <v-container >
+    <v-form v-model="valid" @submit.prevent="submit()" ref="form">
     <v-card
         class="mx-auto my-12"
         max-width="374"
     >
+
       <v-card-title>Add city</v-card-title>
       <v-card-text>
         <v-row>
-          <strong>Name: </strong><v-text-field v-model="name"></v-text-field>
+          <v-text-field v-model="name"
+          :rules="nameRules"
+          label="Name">
+          </v-text-field>
         </v-row>
         <v-row>
-          <strong>Location: </strong><v-text-field v-model="location"></v-text-field>
+          <v-text-field v-model="location"
+          :rules="locationRules" label="Location"
+          ></v-text-field>
         </v-row>
         <v-row>
-          <strong>Population: </strong><v-text-field v-model="population"></v-text-field>
+          <v-text-field v-model="population"
+          :rules="populationRules" label="Population"
+          ></v-text-field>
         </v-row>
         <v-row>
-          <strong>Area: </strong><v-text-field v-model="area"></v-text-field>
+          <v-text-field v-model="area"
+          :rules="areaRules"
+                        label="Area"
+          ></v-text-field>
         </v-row>
         <v-row>
-          <strong>Found: </strong><v-text-field v-model="found"></v-text-field>
+          <v-text-field v-model="found"
+          :rules="foundRules"
+          label="Found"></v-text-field>
         </v-row>
         <template v-if="addingStatus">
-          <v-row>
+          <v-row>valid: false,
             <v-alert
             >{{addingStatus}}</v-alert>
           </v-row>
@@ -32,7 +46,7 @@
         <v-btn
             color="orange"
             text
-            @click="add()"
+            type="submit"
         >
           Add
         </v-btn>
@@ -46,6 +60,7 @@
 
       </v-card-actions>
     </v-card>
+    </v-form>
   </v-container>
 </template>
 
@@ -54,17 +69,39 @@ import {mapActions} from "vuex";
 
 export default {
   data: ()=> ({
+    valid: false,
     name: '',
     location: '',
     population: '',
     area: '',
     found: '',
-    addingStatus: ''
+    addingStatus: '',
+    nameRules: [
+        v => v.trim().length > 0 || 'Name is required'
+    ],
+    locationRules: [
+      v => v.trim().length > 0 || 'Location is required'
+    ],
+    populationRules: [
+      v => v.trim().length > 0 || 'Population is required'
+    ],
+    areaRules: [
+      v => v.trim().length > 0 || 'Area is required'
+    ],
+    foundRules: [
+       v => v.trim().length > 0 || 'Found is required'
+    ]
   }),
   computed: {
   },
   methods: {
     ...mapActions(['addCity']),
+    submit(){
+      this.$refs.form.validate();
+      if(this.valid) {
+        this.add();
+      }
+    },
     async add() {
       let city ={};
       city.name = this.name;
